24d55dedfa1cc2d9fd88a9bb25e67c289965ce05
mukabyl <mukabyl@yamdex.ru>
2023-01-15 18:04:38 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Class creatClass = new Class(5, "Soz", new int[]{7,5,86,45,85,4,1,25});
+        System.out.println("Number: " + creatClass.getNumber());
+        System.out.println("World: " + creatClass.getWord());
+        System.out.println("Array: " + Arrays.toString(creatClass.getArray()));
+
+
+
+
     }
 }
